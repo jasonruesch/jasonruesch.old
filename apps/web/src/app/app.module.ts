7b21c44bb25deccf652760763a06cfb91dade3d4
Jason Ruesch <jruesch@degreed.com>
2021-11-15 11:05:29 -0500

    chore: add angular material and themes

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LogoComponent } from './logo/logo.component';
+import { IconComponent } from './icon/icon.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LogoComponent, IconComponent],
   imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
