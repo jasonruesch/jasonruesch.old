88196c5c2ea501e189bdc5ba68abcf9f14d3b7c7
Jason Ruesch <jruesch@degreed.com>
2021-11-15 22:08:43 -0500

    feat: add send mail endpoint to api

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { ISendMailOptions } from '@nestjs-modules/mailer';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 import { Message } from '@jasonruesch/api-interfaces';
-
 import { AppService } from './app.service';
 
 @Controller()
@@ -11,5 +11,10 @@ export class AppController {
   @Get('hello')
   getData(): Message {
     return this.appService.getData();
+  }
+
+  @Post('send-mail')
+  sendMail(@Body() options: ISendMailOptions): Promise<unknown> {
+    return this.appService.sendMail(options);
   }
 }
--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { environment } from './../environments/environment';
+import { join } from 'path';
+import * as functions from 'firebase-functions';
 
 @Module({
-  imports: [],
+  imports: [
+    MailerModule.forRoot({
+      ...functions.config().mail,
+      preview: !environment.production,
+      template: {
+        dir: join(__dirname, 'assets/templates'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
