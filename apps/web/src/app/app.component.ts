9ec1dd04b68c2e21a2a616643cdcc423b1448e34
Jason Ruesch <jruesch@degreed.com>
2021-11-17 13:58:02 -0500

    feat: reload when update is available

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, HostBinding } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Message } from '@jasonruesch/api-interfaces';
 import pkg from '../../../../package.json';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'jr-root',
@@ -11,8 +12,18 @@ import pkg from '../../../../package.json';
 export class AppComponent {
   @HostBinding('[attr.jr-version]') version = pkg?.version;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, updates: SwUpdate) {
     // Test the API
     this.http.get<Message>('/api/hello').subscribe(console.log);
+
+    updates.available.subscribe((event) => {
+      console.log('current version is', event.current);
+      console.log('available version is', event.available);
+      updates.activateUpdate().then(() => document.location.reload());
+    });
+    updates.activated.subscribe((event) => {
+      console.log('old version was', event.previous);
+      console.log('new version is', event.current);
+    });
   }
 }
