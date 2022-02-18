bec5a99b813fc778c17b2acaaa6e1e756105133c
Jason Ruesch <jruesch@degreed.com>
2022-02-18 10:54:16 -0500

    fix: lint errors

--- libs/web/feature-home/src/lib/home/home.component.ts
+++ libs/web/feature-home/src/lib/home/home.component.ts
@@ -1,16 +1,9 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'jr-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class HomeComponent {}
