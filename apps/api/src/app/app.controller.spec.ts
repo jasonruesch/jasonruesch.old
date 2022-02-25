5784a3f10590f7d1ff723e79f4c347aae4c07879
Jason Ruesch <jruesch@degreed.com>
2022-02-25 12:27:22 -0500

    ci: add coveralls (#8)
    * ci: add coveralls
    
    * chore: replace styles.fill with fill attributes
    
    * test: add coverage to ci workflow
    
    * chore: change fill attributes back to style.fill
    
    * ci: test api only to test coverage
    
    * ci: set git commit for coverage
    
    * ci: ignore some paths
    
    * ci: ignore some paths
    
    * test: add more coverage

--- apps/api/src/app/app.controller.spec.ts
+++ apps/api/src/app/app.controller.spec.ts
@@ -6,17 +6,34 @@ import { AppService } from './app.service';
 describe('AppController', () => {
   let app: TestingModule;
 
+  const mockAppService = {
+    getData: jest.fn().mockReturnValue({ message: 'Test message' }),
+    sendMail: jest.fn(),
+  };
+
   beforeAll(async () => {
     app = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: AppService,
+          useValue: mockAppService,
+        },
+      ],
     }).compile();
   });
 
   describe('getData', () => {
     it('should return "Welcome to api!"', () => {
       const appController = app.get<AppController>(AppController);
-      expect(appController.getData()).toEqual({ message: 'Welcome to api!' });
+      expect(appController.getData()).toEqual({ message: 'Test message' });
+    });
+
+    it('should send an email', () => {
+      const appController = app.get<AppController>(AppController);
+      appController.sendMail({});
+      expect(mockAppService.sendMail).toHaveBeenCalled();
     });
   });
 });
