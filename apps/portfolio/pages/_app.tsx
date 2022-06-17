e3626ab9f49b6b7357a1834c50a0552b67b0b2c0
Jason Ruesch <jruesch@degreed.com>
2022-06-17 15:59:30 -0400

    refactor: create beams component

--- apps/portfolio/pages/_app.tsx
+++ apps/portfolio/pages/_app.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 
 import { ThemeProvider } from 'next-themes';
 
-import { Layout, Navbar } from '../components';
+import { Beams, Layout, Navbar } from '../components';
 
 import './styles.css';
 
@@ -26,34 +26,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
       >
         {isHydrated && (
           <>
-            {/* Beams */}
-            <div className="pointer-events-none absolute inset-x-0 top-0 z-10 flex justify-center">
-              <div className="flex w-[108rem] flex-none justify-end">
-                <picture>
-                  <source
-                    srcSet="/images/beams/docs@30.avif"
-                    type="image/avif"
-                  />
-                  <img
-                    src="/images/beams/docs@tinypng.png"
-                    alt=""
-                    className="w-[71.75rem] max-w-none flex-none dark:hidden"
-                  />
-                </picture>
-                <picture>
-                  <source
-                    srcSet="/images/beams/docs-dark@30.avif"
-                    type="image/avif"
-                  />
-                  <img
-                    src="/images/beams/docs-dark@tinypng.png"
-                    alt=""
-                    className="hidden w-[90rem] max-w-none flex-none dark:block"
-                  />
-                </picture>
-              </div>
-            </div>
-
+            <Beams />
             <Navbar />
             <Layout>
               <Component {...pageProps} />
