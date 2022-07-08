4633071a8e79b9c5d9cd8a91a403fdfbd2bd6712
Jason Ruesch <jruesch@degreed.com>
2022-07-08 11:04:55 -0400

    fix: back to top button scrolling

--- apps/portfolio/components/BackToTopButton.tsx
+++ apps/portfolio/components/BackToTopButton.tsx
@@ -1,7 +1,6 @@
 import { Fragment, useEffect, useState } from 'react';
 import { Transition } from '@headlessui/react';
 import { ChevronUpIcon } from '@heroicons/react/outline';
-import { animateScroll as scroll } from 'react-scroll';
 import useScrollOffset from '@/hooks/useScrollOffset';
 
 export function BackToTopButton() {
@@ -40,7 +39,7 @@ export function BackToTopButton() {
     >
       <button
         className="focus:ring-primary-500 dark:focus:ring-primary-400 bg-primary-600 hover:bg-primary-700 disabled:bg-primary-300 dark:bg-primary-500 dark:hover:bg-primary-600 dark:disabled:bg-primary-300 fixed bottom-8 right-12 z-40 inline-flex items-center justify-center rounded-full border border-transparent p-6 text-base font-medium text-black shadow-sm hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:text-neutral-500 dark:disabled:text-neutral-600 print:hidden"
-        onClick={() => scroll.scrollToTop()}
+        onClick={() => window.scrollTo(0, 0)}
       >
         <ChevronUpIcon className="h-6 w-6" />
       </button>
