47c959210245f69e775525cefbae70fa95bf0590
Jason Ruesch <jruesch@degreed.com>
2022-07-20 22:33:46 -0400

    chore: add tailwind css

--- apps/portfolio/pages/_app.tsx
+++ apps/portfolio/pages/_app.tsx
@@ -1,16 +1,14 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
+import '../styles/tailwind.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to portfolio!</title>
+        <title>Jason Ruesch</title>
       </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
+      <Component {...pageProps} />
     </>
   );
 }
---
+++ apps/portfolio/tailwind.config.js
@@ -0,0 +1,14 @@
+// apps/app1/tailwind.config.js
+const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
+const { join } = require('path');
+
+module.exports = {
+  content: [
+    join(__dirname, '{pages,components}/**/*!(*.stories|*.spec).{ts,tsx,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
