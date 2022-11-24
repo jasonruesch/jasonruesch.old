5879e28f431e22ce1023c048eeb6cda6a13134f7
Jason Ruesch <jruesch@degreed.com>
2022-11-24 14:14:53 -0500

    chore: root configurations for the Spotlight template

---
+++ components/Container.tsx
@@ -0,0 +1,39 @@
+import { forwardRef } from 'react';
+import clsx from 'clsx';
+
+export const OuterContainer = forwardRef(function OuterContainer(
+  { className, children, ...props }: any,
+  ref
+) {
+  return (
+    <div ref={ref} className={clsx('sm:px-8', className)} {...props}>
+      <div className="mx-auto max-w-7xl lg:px-8">{children}</div>
+    </div>
+  );
+});
+
+export const InnerContainer = forwardRef(function InnerContainer(
+  { className, children, ...props }: any,
+  ref
+) {
+  return (
+    <div
+      ref={ref}
+      className={clsx('relative px-4 sm:px-8 lg:px-12', className)}
+      {...props}
+    >
+      <div className="mx-auto max-w-2xl lg:max-w-5xl">{children}</div>
+    </div>
+  );
+});
+
+export const Container = forwardRef(function Container(
+  { children, ...props }: any,
+  ref
+) {
+  return (
+    <OuterContainer ref={ref} {...props}>
+      <InnerContainer>{children}</InnerContainer>
+    </OuterContainer>
+  );
+});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,39 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useEffect, useRef } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { Footer } from '@/components/Footer';
+import { Header } from '@/components/Header';
+
+import '@/styles/tailwind.css';
+import 'focus-visible';
+import { AppProps } from 'next/app';
+
+function usePrevious(value: string) {
+  let ref = useRef<string>();
+
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+
+  return ref.current;
+}
+
+export default function App({ Component, pageProps, router }: AppProps) {
+  let previousPathname = usePrevious(router.pathname);
+
+  return (
+    <>
+      <div className="fixed inset-0 flex justify-center sm:px-8">
+        <div className="flex w-full max-w-7xl lg:px-8">
+          <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+        </div>
+      </div>
+      <div className="relative">
+        <Header />
+        <main>
+          <Component previousPathname={previousPathname} {...pageProps} />
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
 }
