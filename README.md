0587afe402b2984e6b6bfa090dee515afe7acfd7
Jason Ruesch <jason.ruesch@me.com>
2023-01-19 16:20:00 -0500

    chore: fix name and e2e test

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Jasonruesch
+# Jason Ruesch
 
 <a href="https://nx.dev" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png" width="45"></a>
 
--- e2e/src/e2e/app.cy.ts
+++ e2e/src/e2e/app.cy.ts
@@ -8,6 +8,6 @@ describe('jasonruesch', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome jasonruesch');
+    getGreeting().contains('Jason Ruesch');
   });
 });
