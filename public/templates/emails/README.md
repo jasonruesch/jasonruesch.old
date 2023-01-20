0ead309b559307527f32e228b75ab3f0a91d512d
Jason Ruesch <jason.ruesch@me.com>
2023-01-19 22:33:55 -0500

    feat: add maizzle for email template development

--- public/templates/emails/README.md
+++ public/templates/emails/README.md
@@ -3,5 +3,5 @@ This folder contains the email templates from the build output of the emails app
 To build the templates and have them placed here, run the following:
 
 ```
-nx build emails
+npm run build:emails
 ```
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
 const { join } = require('path');
 const defaultTheme = require('tailwindcss/defaultTheme');
-const colors = require('tailwindcss/colors');
+const workspacePreset = require('./tailwind-workspace-preset');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [workspacePreset],
   content: [
     join(__dirname, 'index.html'),
     join(
@@ -18,26 +19,6 @@ module.exports = {
     fontFamily: {
       sans: ['Inter', ...defaultTheme.fontFamily.sans],
       display: ['Alegreya Sans SC', ...defaultTheme.fontFamily.sans],
-    },
-    colors: {
-      transparent: colors.transparent,
-      current: colors.current,
-      inherit: colors.inherit,
-      black: colors.black,
-      white: colors.white,
-      cyan: colors.cyan,
-      fuchsia: colors.fuchsia,
-      violet: colors.violet,
-      teal: colors.teal,
-      neutral: colors.neutral,
-      red: colors.red,
-      green: colors.green,
-    },
-    screens: {
-      ...defaultTheme.screens,
-      sm: {
-        raw: `(min-width: ${defaultTheme.screens.sm}) and (min-height: ${defaultTheme.screens.sm})`,
-      },
     },
     extend: {},
   },
