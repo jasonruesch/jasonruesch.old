353351f61aa6a26f379d9f255410617c0d78adca
Jason Ruesch <jason.ruesch@me.com>
2023-01-24 10:37:05 -0500

    SAVEPOINT

--- shared/ui/src/lib/PageTransitions.tsx
+++ shared/ui/src/lib/PageTransitions.tsx
@@ -4,111 +4,80 @@ import {
   useReducedMotion,
   Variants,
 } from 'framer-motion';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import useWindowSize from './hooks/useWindowSize';
 
-const DURATION = 1;
-const DELAY = 1.25;
-const SCALE = 0.6;
-const BORDER_RADIUS = '16px';
-
-// const variants: Variants = {
-//   in: {
-//     height: '100vh',
-//     // width: '100vh',
-//     overflow: 'hidden',
-//     scale: SCALE,
-//     opacity: 0,
-//     x: '100vw',
-//     borderRadius: BORDER_RADIUS,
-//     transition: {
-//       duration: 0,
-//     },
-//   },
-//   center: {
-//     opacity: 1,
-//     x: 0,
-//     // x: 'calc((100vw / 2) - (100vh / 2))',
-//     transition: {
-//       duration: DURATION,
-//     },
-//   },
-//   scaleUp: {
-//     // width: 'auto',
-//     scale: 1,
-//     borderRadius: 0,
-//     transition: {
-//       duration: DURATION,
-//       delay: DELAY,
-//     },
-//   },
-//   resetPage: {
-//     height: 'auto',
-//     overflow: 'visible',
-//     transition: {
-//       duration: 0.1,
-//       delay: DURATION + DELAY,
-//     },
-//   },
-//   adjustPage: {
-//     height: '100vh',
-//     overflow: 'hidden',
-//     transition: {
-//       duration: 0,
-//     },
-//   },
-//   scaleDown: {
-//     // width: '100vh',
-//     scale: SCALE,
-//     borderRadius: BORDER_RADIUS,
-//     transition: {
-//       duration: DURATION,
-//     },
-//   },
-//   out: {
-//     opacity: 0,
-//     x: '-100vw',
-//     transition: {
-//       duration: DURATION,
-//       delay: DELAY,
-//     },
-//   },
-// };
+const MULTIPLIER = 1;
 
 const variants: Variants = {
-  enter: {
-    x: '100vw',
+  hidden: ({ didNavigate }) =>
+    didNavigate
+      ? {
+          x: '100vw',
+          opacity: 0,
+          overflow: 'hidden',
+          height: '100vh',
+          width: '100vh',
+          scale: 0.6,
+          borderRadius: '16px',
+        }
+      : {},
+  enter: ({ windowSize, didNavigate }) =>
+    didNavigate
+      ? {
+          x: [
+            `${windowSize.width}px`,
+            `${windowSize.width / 2 - windowSize.height / 2}px`,
+            '0px',
+          ],
+          opacity: 1,
+          overflow: 'visible',
+          height: 'auto',
+          width: 'auto',
+          scale: 1,
+          borderRadius: '0',
+          transition: {
+            x: {
+              type: 'spring',
+              stiffness: 300,
+              damping: 30,
+              duration: 0.8 * MULTIPLIER,
+              times: [0, 0.5, 1],
+            },
+            opacity: { duration: 0.2 * MULTIPLIER },
+            overflow: { delay: 0.8 * MULTIPLIER, duration: 0.1 },
+            height: { delay: 0.8 * MULTIPLIER, duration: 0.1 },
+            default: { delay: 0.4 * MULTIPLIER, duration: 0.4 * MULTIPLIER },
+          },
+        }
+      : {},
+  exit: ({ windowSize }) => ({
+    x: [
+      '0px',
+      `${windowSize.width / 2 - windowSize.height / 2}px`,
+      `-${windowSize.width}px`,
+    ],
     opacity: 0,
+    overflow: 'hidden',
+    height: '100vh',
+    width: '100vh',
     scale: 0.6,
-  },
-  center: {
-    zIndex: 1,
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      x: { type: 'spring', stiffness: 300, damping: 30, duration: 0.4 },
-      opacity: { duration: 0.2 },
-      scale: { delay: 0.4, duration: 0.4 },
-    },
-  },
-  exit: {
-    zIndex: 0,
-    x: '-100vw',
-    opacity: 0,
-    scale: 0.6,
+    borderRadius: '16px',
     transition: {
       x: {
         type: 'spring',
         stiffness: 300,
         damping: 30,
-        delay: 0.4,
-        duration: 0.4,
+        duration: 0.8 * MULTIPLIER,
+        times: [0, 0.5, 1],
       },
-      opacity: { delay: 0.4, duration: 0.2 },
-      scale: { duration: 0.4 },
+      opacity: { delay: 0.6 * MULTIPLIER, duration: 0.2 * MULTIPLIER },
+      overflow: { duration: 0 },
+      height: { duration: 0 },
+      default: { duration: 0.4 * MULTIPLIER },
     },
-  },
+  }),
 };
 
 export interface PageTransitionsProps {
@@ -124,8 +93,19 @@ export const PageTransitions = ({
   children,
   className,
 }: PageTransitionsProps) => {
-  const { pathname: route } = useLocation();
   const shouldReduceMotion = useReducedMotion();
+  const { pathname } = useLocation();
+  const [previousPathname, setPreviousPathname] = useState(pathname);
+  const [didNavigate, setDidNavigate] = useState(false);
+  const windowSize = useWindowSize();
+
+  useEffect(() => {
+    if (previousPathname !== pathname) {
+      setDidNavigate(true);
+    }
+
+    setPreviousPathname(pathname);
+  }, [pathname, previousPathname]);
 
   return (
     <div
@@ -134,26 +114,30 @@ export const PageTransitions = ({
       after:to-fuchsia-500 dark:after:from-neutral-800 dark:after:via-violet-400 dark:after:to-teal-400"
     >
       <AnimatePresence
-        initial={false}
+        initial={false} // Disabled for now because the animate keyframes are running when the page loads
         mode="wait"
         onExitComplete={() => window.scrollTo(0, 0)}
       >
         <motion.div
           id="page"
-          key={route}
+          key={pathname}
           className={className}
+          custom={{
+            windowSize,
+            didNavigate,
+          }}
           variants={!shouldReduceMotion ? variants : undefined}
-          initial="enter"
-          // animate={['center', 'scaleUp', 'resetPage']}
-          // exit={['adjustPage', 'scaleDown', 'out']}
-          animate="center"
+          initial="hidden"
+          animate="enter"
           exit="exit"
           onAnimationComplete={(definition) => {
-            if (definition === 'resetPage') {
+            if (definition === 'enter') {
               setTimeout(() => {
                 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
                 const page = document.getElementById('page')!;
                 page.style.transform = 'none';
+
+                setDidNavigate(false);
               }, 100);
             }
           }}
