4bc2b4dadad0adfd6d4b41946b43fa219e600c69
Jason Ruesch <jason.ruesch@me.com>
2023-01-23 21:09:07 -0500

    SAVEPOINT

--- shared/ui/src/lib/PageTransitions.tsx
+++ shared/ui/src/lib/PageTransitions.tsx
@@ -7,70 +7,106 @@ import {
 import { ReactNode } from 'react';
 import { useLocation } from 'react-router-dom';
 
-const DURATION = 0.4;
-const DELAY = 0.5;
+const DURATION = 1;
+const DELAY = 1.25;
 const SCALE = 0.6;
 const BORDER_RADIUS = '16px';
 
+// const variants: Variants = {
+//   in: {
+//     height: '100vh',
+//     // width: '100vh',
+//     overflow: 'hidden',
+//     scale: SCALE,
+//     opacity: 0,
+//     x: '100vw',
+//     borderRadius: BORDER_RADIUS,
+//     transition: {
+//       duration: 0,
+//     },
+//   },
+//   center: {
+//     opacity: 1,
+//     x: 0,
+//     // x: 'calc((100vw / 2) - (100vh / 2))',
+//     transition: {
+//       duration: DURATION,
+//     },
+//   },
+//   scaleUp: {
+//     // width: 'auto',
+//     scale: 1,
+//     borderRadius: 0,
+//     transition: {
+//       duration: DURATION,
+//       delay: DELAY,
+//     },
+//   },
+//   resetPage: {
+//     height: 'auto',
+//     overflow: 'visible',
+//     transition: {
+//       duration: 0.1,
+//       delay: DURATION + DELAY,
+//     },
+//   },
+//   adjustPage: {
+//     height: '100vh',
+//     overflow: 'hidden',
+//     transition: {
+//       duration: 0,
+//     },
+//   },
+//   scaleDown: {
+//     // width: '100vh',
+//     scale: SCALE,
+//     borderRadius: BORDER_RADIUS,
+//     transition: {
+//       duration: DURATION,
+//     },
+//   },
+//   out: {
+//     opacity: 0,
+//     x: '-100vw',
+//     transition: {
+//       duration: DURATION,
+//       delay: DELAY,
+//     },
+//   },
+// };
+
 const variants: Variants = {
-  in: {
-    height: '100vh',
-    // width: '100vh',
-    overflow: 'hidden',
-    scale: SCALE,
-    opacity: 0,
-    originX: 0.5,
+  enter: {
     x: '100vw',
-    borderRadius: BORDER_RADIUS,
-    transition: {
-      duration: 0,
-    },
+    opacity: 0,
+    scale: 0.6,
   },
   center: {
-    opacity: 1,
+    zIndex: 1,
     x: 0,
-    transition: {
-      duration: DURATION,
-    },
-  },
-  scaleUp: {
-    // width: 'auto',
+    opacity: 1,
     scale: 1,
-    borderRadius: 0,
     transition: {
-      duration: DURATION,
-      delay: DELAY,
+      x: { type: 'spring', stiffness: 300, damping: 30, duration: 0.4 },
+      opacity: { duration: 0.2 },
+      scale: { delay: 0.4, duration: 0.4 },
     },
   },
-  resetPage: {
-    height: 'auto',
-    overflow: 'visible',
-    transition: {
-      duration: 0.1,
-      delay: DURATION + DELAY,
-    },
-  },
-  adjustPage: {
-    height: '100vh',
-    overflow: 'hidden',
-    transition: {
-      duration: 0,
-    },
-  },
-  scaleDown: {
-    // width: '100vh',
-    scale: SCALE,
-    borderRadius: BORDER_RADIUS,
-    transition: {
-      duration: DURATION,
-    },
-  },
-  out: {
-    opacity: 0,
+  exit: {
+    zIndex: 0,
     x: '-100vw',
+    opacity: 0,
+    scale: 0.6,
     transition: {
-      duration: DURATION,
-      delay: DELAY,
+      x: {
+        type: 'spring',
+        stiffness: 300,
+        damping: 30,
+        delay: 0.4,
+        duration: 0.4,
+      },
+      opacity: { delay: 0.4, duration: 0.2 },
+      scale: { duration: 0.4 },
     },
   },
 };
@@ -106,10 +142,12 @@ export const PageTransitions = ({
           id="page"
           key={route}
           className={className}
-          initial="in"
-          animate={['center', 'scaleUp', 'resetPage']}
-          exit={['adjustPage', 'scaleDown', 'out']}
           variants={!shouldReduceMotion ? variants : undefined}
+          initial="enter"
+          // animate={['center', 'scaleUp', 'resetPage']}
+          // exit={['adjustPage', 'scaleDown', 'out']}
+          animate="center"
+          exit="exit"
           onAnimationComplete={(definition) => {
             if (definition === 'resetPage') {
               setTimeout(() => {
