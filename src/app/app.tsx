e4e3495421ecf594718ad1a2bf4bfaa0857a9cc3
Jason Ruesch <jruesch@degreed.com>
2023-01-29 22:12:28 -0500

    SAVEPOINT

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
 import { ThemeProvider } from 'next-themes';
-import { useOutlet } from 'react-router-dom';
-
+import { useLocation, useNavigate, useOutlet } from 'react-router-dom';
+import { useSwipeable } from 'react-swipeable';
 import { Beams, Header, Navbar, PageTransitions } from '@jasonruesch/shared/ui';
+import { eventBus } from '@jasonruesch/shared/utils';
 
 const AnimatedOutlet: React.FC = () => {
   const o = useOutlet();
@@ -16,6 +17,35 @@ export function App() {
   useEffect(() => {
     setIsHydrated(true);
   }, []);
+
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const navigateForward = (current: string) => {
+    if (current === '/') {
+      eventBus.dispatch('intendToNavigate', { to: '/about' });
+      navigate('/about');
+    } else if (current === '/about') {
+      eventBus.dispatch('intendToNavigate', { to: '/contact' });
+      navigate('/contact');
+    }
+  };
+
+  const navigateBackward = (current: string) => {
+    if (current === '/contact') {
+      eventBus.dispatch('intendToNavigate', { to: '/about' });
+      navigate('/about');
+    } else if (current === '/about') {
+      eventBus.dispatch('intendToNavigate', { to: '/' });
+      navigate('/');
+    }
+  };
+
+  const swipeHandlers = useSwipeable({
+    onSwipedLeft: () => navigateForward(pathname),
+    onSwipedRight: () => navigateBackward(pathname),
+    preventScrollOnSwipe: true,
+  });
 
   return !isHydrated ? null : (
     <ThemeProvider
@@ -33,7 +63,10 @@ export function App() {
       </Header>
       <Beams className="z-10" />
       <PageTransitions>
-        <main className="flex min-h-screen bg-neutral-100 text-neutral-900 dark:bg-neutral-800 dark:text-neutral-50">
+        <main
+          className="flex min-h-screen bg-neutral-100 text-neutral-900 dark:bg-neutral-800 dark:text-neutral-50"
+          {...swipeHandlers}
+        >
           <div className="z-20 mx-auto w-full max-w-screen-lg px-4 sm:px-8">
             <AnimatedOutlet />
           </div>
