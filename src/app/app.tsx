d9aa13688f9a0a48c7e63497c8280374d857448b
Jason Ruesch <jruesch@degreed.com>
2023-07-20 21:48:39 -0400

    chore(jasonruesch): clean slate

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-
-import NxWelcome from './nx-welcome';
-
 export function App() {
-  return (
-    <div>
-      <NxWelcome title="jasonruesch" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
