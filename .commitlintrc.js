474327bb99414af8402c28308fdc15e63252241c
Jason Ruesch <jruesch@degreed.com>
2023-11-12 13:17:45 -0500

    chore(repo): add husky and semantic release for conventional commits

---
+++ .commitlintrc.js
@@ -0,0 +1,21 @@
+const {
+  utils: { getProjects },
+} = require('@commitlint/config-nx-scopes');
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': async (ctx) => [
+      2,
+      'always',
+      [
+        ...(await getProjects(
+          ctx,
+          ({ name, projectType }) => !name.includes('e2e')
+        )),
+        'repo',
+        'docs',
+      ],
+    ],
+  },
+};
