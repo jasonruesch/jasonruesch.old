ed35409a459092a1dee46267ebcc5600d5f44bde
Jason Ruesch <jruesch@degreed.com>
2023-11-13 14:42:35 -0500

    chore(repo): reset releases

--- release.config.js
+++ release.config.js
@@ -55,11 +55,17 @@ module.exports = {
       },
     ],
     [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
+    [
       '@semantic-release/git',
       {
-        assets: ['package.json', 'CHANGELOG.md'],
+        assets: ['package*.json', 'CHANGELOG.md'],
         message:
-          'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
   ],
