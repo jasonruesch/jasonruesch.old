1205686dbb53db1826faf97ff43580a480f734c8
Jason Ruesch <jruesch@degreed.com>
2023-11-13 13:17:26 -0500

    chore(repo): add Tailwind CSS to both apps

--- libs/projects/src/lib/project-list/project-list.tsx
+++ libs/projects/src/lib/project-list/project-list.tsx
@@ -1,12 +1,7 @@
-import styles from './project-list.module.css';
-
-/* eslint-disable-next-line */
-export interface ProjectListProps {}
-
-export function ProjectList(props: ProjectListProps) {
+export function ProjectList() {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to ProjectList!</h1>
+    <div>
+      <h1 className="text-4xl font-bold">Welcome to ProjectList!</h1>
       <p>This is a change.</p>
     </div>
   );
