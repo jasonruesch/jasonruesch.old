aaaa7ccc25a529eeaf64bd9cf76358ee81ebf478
Jason Ruesch <jruesch@degreed.com>
2023-11-17 22:47:00 -0500

    chore(repo): skip workflow run for release commits

--- release.config.js
+++ release.config.js
@@ -65,7 +65,7 @@ module.exports = {
       {
         assets: ['package*.json', 'CHANGELOG.md'],
         message:
-          'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
   ],
