e16d52cfa3a62a050346c939e7450e0a0b44f773
Jason Ruesch <jruesch@degreed.com>
2023-11-17 19:47:52 -0500

    ci(repo): fix coverage generation

--- apps/portfolio/src/app/app.spec.tsx
+++ apps/portfolio/src/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
-    expect(getByText(/Welcome portfolio/gi)).toBeTruthy();
+    expect(getByText(/Welcome portfolio/i)).toBeTruthy();
   });
 });
