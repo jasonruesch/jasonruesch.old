35dc6ffc7cf56d3c6c1e7b55038f66eb00ee56d7
Jason Ruesch <jruesch@degreed.com>
2023-11-20 17:11:30 -0500

    feat(portfolio): add localforage, match-sorter and sorty-by

--- libs/portfolio/data-access/src/lib/projects/project.api.spec.ts
+++ libs/portfolio/data-access/src/lib/projects/project.api.spec.ts
@@ -2,6 +2,33 @@ import { getProjects } from './project.api';
 
 describe('portfolioDataAccess', () => {
   it('should work', async () => {
+    vi.mock('localforage', async () => {
+      return {
+        default: {
+          getItem: async () => [
+            {
+              id: 'one',
+              name: 'Project One',
+              description: 'Project One Description',
+              tags: ['one'],
+            },
+            {
+              id: 'two',
+              name: 'Project Two',
+              description: 'Project Two Description',
+              tags: ['two'],
+            },
+            {
+              id: 'three',
+              name: 'Project Three',
+              description: 'Project Three Description',
+              tags: ['three'],
+            },
+          ],
+        },
+      };
+    });
+
     expect(await getProjects()).toEqual([
       {
         id: 'one',
