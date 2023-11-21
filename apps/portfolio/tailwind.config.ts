071173cd7d38e4c158c58141a260a39dc3369ddb
Jason Ruesch <jruesch@degreed.com>
2023-11-20 23:09:11 -0500

    ci(repo): update release job

--- apps/portfolio/tailwind.config.ts
+++ apps/portfolio/tailwind.config.ts
@@ -1,8 +1,10 @@
+import tailwindPreset from '@jasonruesch/tailwind';
 import { createGlobPatternsForDependencies } from '@nx/react/tailwind';
 import { join } from 'path';
 import type { Config } from 'tailwindcss';
 
 export default {
+  presets: [tailwindPreset],
   content: [
     join(__dirname, 'index.html'),
     join(
---
+++ libs/tailwind/release.config.js
@@ -0,0 +1,71 @@
+const srcRoot = 'libs/tailwind';
+
+module.exports = {
+  extends: 'semantic-release-npm-github-publish',
+  pkgRoot: `dist/${srcRoot}`,
+  commitPaths: [`${srcRoot}/*`],
+  branches: [
+    'main',
+    {
+      name: 'part*',
+      prerelease: true,
+    },
+  ],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'docs', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'style', release: 'patch' },
+          { type: 'ci', release: 'patch' },
+        ],
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+        },
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+        },
+        writerOpts: {
+          commitsSort: ['subject', 'scope'],
+        },
+        presetConfig: {
+          types: [
+            { type: 'feat', section: 'Features' },
+            { type: 'fix', section: 'Bug Fixes' },
+            { type: 'chore', section: 'Other' },
+            { type: 'docs', section: 'Other' },
+            { type: 'style', section: 'Style' },
+            { type: 'refactor', section: 'Other' },
+            { type: 'perf', hidden: false },
+            { type: 'test', hidden: false },
+            { type: 'ci', section: 'Other' },
+          ],
+        },
+      },
+    ],
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: `${srcRoot}/CHANGELOG.md`,
+      },
+    ],
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: [`${srcRoot}/package.json`, `${srcRoot}/CHANGELOG.md`],
+        message:
+          'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
